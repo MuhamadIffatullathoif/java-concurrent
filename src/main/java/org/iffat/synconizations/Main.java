b649cd71ad1256318f8517c9bff398da771b2a9d
MuhamadIffatullathoif <iffatlathoif1997@gmail.com>
2024-04-09 12:09:52 +0700

    09. Synchronization, synchronized blocks

--- src/main/java/org/iffat/synconizations/Main.java
+++ src/main/java/org/iffat/synconizations/Main.java
@@ -4,15 +4,20 @@ public class Main {
 
 	public static void main(String[] args) {
 
-		BankAccount companyAccount = new BankAccount(10_000);
+		BankAccount companyAccount = new BankAccount("Tom", 10_000);
 
 		Thread thread1 = new Thread(() -> companyAccount.withDraw(2500));
 		Thread thread2 = new Thread(() -> companyAccount.deposit(5000));
-		Thread thread3 = new Thread(() -> companyAccount.withDraw(2500));
+		Thread thread3 = new Thread(() -> companyAccount.setName("Tim"));
 		Thread thread4 = new Thread(() -> companyAccount.withDraw(5000));
 
 		thread1.start();
 		thread2.start();
+		try {
+			Thread.sleep(500);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
 		thread3.start();
 		thread4.start();
 
